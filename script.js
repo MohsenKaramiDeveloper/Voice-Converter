9033fddaf536dbc12c1afe0a1ab3159aa0517905
Mohsen Karami <kara0158@algonquinlive.com>
2023-11-14 22:51:09 -0500

    test 1

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let speech = new SpeechSynthesisUtterance();
 
-document.querySelector("button").addEventListener("click", () =>{
+document.querySelector("button").addEventListener("click", () => {
     speech.text = document.querySelector("textarea").value;
-    window.speechSynthesis.speak(speech)
-})
+
+    // Get the selected language code from the dropdown
+    let selectedLanguage = document.getElementById("languageSelector").value;
+    
+    // Set the selected language for speech synthesis
+    speech.lang = selectedLanguage;
+
+    // Speak the text with the chosen language
+    window.speechSynthesis.speak(speech);
+});
